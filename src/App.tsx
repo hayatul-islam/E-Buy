622b3d48329abd36643fa19278feb0038507d668
munimrahman <munimrh@gmail.com>
2023-03-16 20:46:17 +0600

    test

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,11 @@ import "./App.css";
 function App() {
   const [count, setCount] = useState(0);
 
-  return <div className="App">Hello world</div>;
+  return (
+    <div className="App">
+      <h1>Hello world</h1>
+    </div>
+  );
 }
 
 export default App;
