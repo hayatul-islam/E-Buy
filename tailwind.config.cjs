aa5411eb953c42c5ebcb955533df92a69b47c1ad
hayatul-islam <hayatulemon340@gmail.com>
2023-03-17 22:00:27 +0600

    tailwind color system add

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#2bbef9",
+        secondary: "#233a95",
+        success: "#00b853",
+        warning: "#ffcd00",
+        danger: "#d51243",
+        info: "",
+      },
+    },
   },
   plugins: [],
 };
